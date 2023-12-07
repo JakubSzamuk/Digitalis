5cc227ff4180d7c44fa6bf2f71373c506fec50e9
parallax <parallax@PRLX00062-MBP.parall.ax>
2023-12-07 13:41:58 +0000

    testing with a websocket example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,184 @@
-mod send_message;
+// mod send_message;
+// use axum::{
+//     routing::{get, post},
+//     Router
+// };
+// use send_message::{
+//     echo,
+//     create_user,
+//     socket_handler, ws_handler
+// };
+
+// #[tokio::main]
+// async fn main() {
+//     let app = Router::new()
+//                         .route("/", get(|| async { "Hello, World!" }))
+//                         .route("/echo", get(echo))
+//                         .route("/hello_world", post(create_user))
+//                         .route("/web_socket_test", get(ws_handler));
+
+//     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+//     axum::serve(listener, app).await.unwrap();
+// }
+
+//! Run with
+//!
+//! ```not_rust
+//! cargo test -p example-testing-websockets
+//! ```
+
 use axum::{
-    routing::{get, post},
-    Router
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::Response,
+    routing::get,
+    Router,
 };
-use send_message::{
-    echo,
-    create_user,
-    socket_handler, ws_handler
-};
+use futures::{Sink, SinkExt, Stream, StreamExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-                        .route("/", get(|| async { "Hello, World!" }))
-                        .route("/echo", get(echo))
-                        .route("/hello_world", post(create_user))
-                        .route("/web_socket_test", get(ws_handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app()).await.unwrap();
+}
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+fn app() -> Router {
+    // WebSocket routes can generally be tested in two ways:
+    //
+    // - Integration tests where you run the server and connect with a real WebSocket client.
+    // - Unit tests where you mock the socket as some generic send/receive type
+    //
+    // Which version you pick is up to you. Generally we recommend the integration test version
+    // unless your app has a lot of setup that makes it hard to run in a test.
+    Router::new()
+        .route("/integration-testable", get(integration_testable_handler))
+        .route("/unit-testable", get(unit_testable_handler))
+}
+
+// A WebSocket handler that echos any message it receives.
+//
+// This one we'll be integration testing so it can be written in the regular way.
+async fn integration_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(integration_testable_handle_socket)
+}
+
+async fn integration_testable_handle_socket(mut socket: WebSocket) {
+    let mut i: u16 = 0;
+    while (true) {
+        socket.send(Message::Text(format!("{}", i))).await;
+        if i > 255 {
+            i = 0;
+        }
+        i += 1;
+    }
+    while let Some(Ok(msg)) = socket.recv().await {
+        if let Message::Text(msg) = msg {
+            if socket
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+
+
+    }
+}
+
+// The unit testable version requires some changes.
+//
+// By splitting the socket into an `impl Sink` and `impl Stream` we can test without providing a
+// real socket and instead using channels, which also implement `Sink` and `Stream`.
+async fn unit_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(|socket| {
+        let (write, read) = socket.split();
+        unit_testable_handle_socket(write, read)
+    })
+}
+
+// The implementation is largely the same as `integration_testable_handle_socket` expect we call
+// methods from `SinkExt` and `StreamExt`.
+async fn unit_testable_handle_socket<W, R>(mut write: W, mut read: R)
+where
+    W: Sink<Message> + Unpin,
+    R: Stream<Item = Result<Message, axum::Error>> + Unpin,
+{
+    while let Some(Ok(msg)) = read.next().await {
+        if let Message::Text(msg) = msg {
+            if write
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        future::IntoFuture,
+        net::{Ipv4Addr, SocketAddr},
+    };
+    use tokio_tungstenite::tungstenite;
+
+    // We can integration test one handler by running the server in a background task and
+    // connecting to it like any other client would.
+    #[tokio::test]
+    async fn integration_test() {
+        let listener = tokio::net::TcpListener::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0)))
+            .await
+            .unwrap();
+        let addr = listener.local_addr().unwrap();
+        tokio::spawn(axum::serve(listener, app()).into_future());
+
+        let (mut socket, _response) =
+            tokio_tungstenite::connect_async(format!("ws://{addr}/integration-testable"))
+                .await
+                .unwrap();
+
+        socket
+            .send(tungstenite::Message::text("foo"))
+            .await
+            .unwrap();
+
+        let msg = match socket.next().await.unwrap().unwrap() {
+            tungstenite::Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+
+    // We can unit test the other handler by creating channels to read and write from.
+    #[tokio::test]
+    async fn unit_test() {
+        // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and
+        // `Stream`
+        let (socket_write, mut test_rx) = futures::channel::mpsc::channel(1024);
+        let (mut test_tx, socket_read) = futures::channel::mpsc::channel(1024);
+
+        tokio::spawn(unit_testable_handle_socket(socket_write, socket_read));
+
+        test_tx
+            .send(Ok(Message::Text("foo".to_owned())))
+            .await
+            .unwrap();
+
+        let msg = match test_rx.next().await.unwrap() {
+            Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
 }
--- backend/src/send_message.rs
+++ backend/src/send_message.rs
@@ -1,83 +1,209 @@
-use std::net::SocketAddr;
+// use std::net::SocketAddr;
 
-use axum::{body::Bytes, http::StatusCode, Json, extract::ws::{Message, WebSocket, WebSocketUpgrade}};
-use serde::Deserialize;
+// use axum::{body::Bytes, http::StatusCode, Json, extract::ws::{Message, WebSocket, WebSocketUpgrade}};
+// use serde::Deserialize;
+// use axum::{
+//     response::IntoResponse,
+//     routing::get,
+//     Router,
+// };
+// use axum_extra::TypedHeader;
+
+// use std::borrow::Cow;
+// use std::ops::ControlFlow;
+// use std::path::PathBuf;
+// use tower_http::{
+//     services::ServeDir,
+//     trace::{DefaultMakeSpan, TraceLayer},
+// };
+
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+// //allows to extract the IP of connecting user
+// use axum::extract::connect_info::ConnectInfo;
+// use axum::extract::ws::CloseFrame;
+
+// //allows to split the websocket stream into separate TX and RX branches
+// use futures::{sink::SinkExt, stream::StreamExt};
+
+
+// pub async fn echo(body: Bytes) -> Result<String, StatusCode> {
+//     if let Ok(string) = String::from_utf8(body.to_vec()) {
+//         Ok(string)
+//     } else {
+//         Err(StatusCode::BAD_REQUEST)
+//     }
+// }
+
+// #[derive(Deserialize)]
+// pub struct create_user {
+//     email: String,
+//     name: String
+// }
+
+// pub async fn create_user(Json(payload): Json<create_user>) -> Result<String, StatusCode> {
+//     if payload.email == "helloWOrld" {
+//         Ok(payload.email)
+//     } else {
+//         Err(StatusCode::BAD_REQUEST)
+//     }
+// }
+
+
+//! Run with
+//!
+//! ```not_rust
+//! cargo test -p example-testing-websockets
+//! ```
+
 use axum::{
-    response::IntoResponse,
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::Response,
     routing::get,
     Router,
 };
-use axum_extra::TypedHeader;
+use futures::{Sink, SinkExt, Stream, StreamExt};
 
-use std::borrow::Cow;
-use std::ops::ControlFlow;
-use std::path::PathBuf;
-use tower_http::{
-    services::ServeDir,
-    trace::{DefaultMakeSpan, TraceLayer},
-};
-
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-//allows to extract the IP of connecting user
-use axum::extract::connect_info::ConnectInfo;
-use axum::extract::ws::CloseFrame;
-
-//allows to split the websocket stream into separate TX and RX branches
-use futures::{sink::SinkExt, stream::StreamExt};
-
-
-pub async fn echo(body: Bytes) -> Result<String, StatusCode> {
-    if let Ok(string) = String::from_utf8(body.to_vec()) {
-        Ok(string)
-    } else {
-        Err(StatusCode::BAD_REQUEST)
-    }
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app()).await.unwrap();
 }
 
-#[derive(Deserialize)]
-pub struct create_user {
-    email: String,
-    name: String
+fn app() -> Router {
+    // WebSocket routes can generally be tested in two ways:
+    //
+    // - Integration tests where you run the server and connect with a real WebSocket client.
+    // - Unit tests where you mock the socket as some generic send/receive type
+    //
+    // Which version you pick is up to you. Generally we recommend the integration test version
+    // unless your app has a lot of setup that makes it hard to run in a test.
+    Router::new()
+        .route("/integration-testable", get(integration_testable_handler))
+        .route("/unit-testable", get(unit_testable_handler))
 }
 
-pub async fn create_user(Json(payload): Json<create_user>) -> Result<String, StatusCode> {
-    if payload.email == "helloWOrld" {
-        Ok(payload.email)
-    } else {
-        Err(StatusCode::BAD_REQUEST)
-    }
+// A WebSocket handler that echos any message it receives.
+//
+// This one we'll be integration testing so it can be written in the regular way.
+async fn integration_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(integration_testable_handle_socket)
 }
 
-
-pub async fn ws_handler(
-    ws: WebSocketUpgrade,
-    user_agent: Option<TypedHeader<headers::UserAgent>>,
-    ConnectInfo(addr): ConnectInfo<SocketAddr>,
-) -> impl IntoResponse {
-    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
-        user_agent.to_string()
-    } else {
-        String::from("Unknown browser")
-    };
-    println!("`{user_agent}` at {addr} connected.");
-    // finalize the upgrade process by returning upgrade callback.
-    // we can customize the callback by sending additional info such as address.
-    ws.on_upgrade(move |socket| socket_handler(socket, addr))
-}
-
-pub async fn socket_handler(mut socket: WebSocket, who: SocketAddr) {
-    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-        println!("Pinged something");
-    }
-
-
-    if let Some(msg) = socket.recv().await {
-        if let Ok(msg) = msg {
-            println!("Message recieved");
-        } else {
-            println!("client {who} abruptly disconnected");
-            return;
+async fn integration_testable_handle_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        if let Message::Text(msg) = msg {
+            if socket
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
         }
+    }
+    let i: u8 = 0;
+    while (true) {
+        socket.send(Message::Text(format!("{}", i)));
+        set_timeout(Duration::from_secs(5)).await;
+        i += 1;
+    }
+}
+
+// The unit testable version requires some changes.
+//
+// By splitting the socket into an `impl Sink` and `impl Stream` we can test without providing a
+// real socket and instead using channels, which also implement `Sink` and `Stream`.
+async fn unit_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(|socket| {
+        let (write, read) = socket.split();
+        unit_testable_handle_socket(write, read)
+    })
+}
+
+// The implementation is largely the same as `integration_testable_handle_socket` expect we call
+// methods from `SinkExt` and `StreamExt`.
+async fn unit_testable_handle_socket<W, R>(mut write: W, mut read: R)
+where
+    W: Sink<Message> + Unpin,
+    R: Stream<Item = Result<Message, axum::Error>> + Unpin,
+{
+    while let Some(Ok(msg)) = read.next().await {
+        if let Message::Text(msg) = msg {
+            if write
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        future::IntoFuture,
+        net::{Ipv4Addr, SocketAddr},
+    };
+    use tokio_tungstenite::tungstenite;
+
+    // We can integration test one handler by running the server in a background task and
+    // connecting to it like any other client would.
+    #[tokio::test]
+    async fn integration_test() {
+        let listener = tokio::net::TcpListener::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0)))
+            .await
+            .unwrap();
+        let addr = listener.local_addr().unwrap();
+        tokio::spawn(axum::serve(listener, app()).into_future());
+
+        let (mut socket, _response) =
+            tokio_tungstenite::connect_async(format!("ws://{addr}/integration-testable"))
+                .await
+                .unwrap();
+
+        socket
+            .send(tungstenite::Message::text("foo"))
+            .await
+            .unwrap();
+
+        let msg = match socket.next().await.unwrap().unwrap() {
+            tungstenite::Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+
+    // We can unit test the other handler by creating channels to read and write from.
+    #[tokio::test]
+    async fn unit_test() {
+        // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and
+        // `Stream`
+        let (socket_write, mut test_rx) = futures::channel::mpsc::channel(1024);
+        let (mut test_tx, socket_read) = futures::channel::mpsc::channel(1024);
+
+        tokio::spawn(unit_testable_handle_socket(socket_write, socket_read));
+
+        test_tx
+            .send(Ok(Message::Text("foo".to_owned())))
+            .await
+            .unwrap();
+
+        let msg = match test_rx.next().await.unwrap() {
+            Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
     }
 }
