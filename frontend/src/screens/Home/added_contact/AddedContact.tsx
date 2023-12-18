31896b3b578d20d956a146e8992c58af7f276f66
Jakub Szamuk <lavaproofJakub1@gmail.com>
2023-12-18 23:19:56 +0000

    Added Contact screen, Most UI elements created, Next is added component logic

--- frontend/src/screens/Home/added_contact/AddedContact.tsx
+++ frontend/src/screens/Home/added_contact/AddedContact.tsx
@@ -1,20 +1,33 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { StandardBackground } from '../../../constants/colors'
+import { Color, StandardBackground } from '../../../constants/colors'
 import { UtilityStyles } from '../../../styles/utility'
 import { FontStyles } from '../../../styles/text'
+import Logo from '../../reusable/Logo'
 
 const AddedContact = () => {
   return (
     <SafeAreaView>
       <StandardBackground style={UtilityStyles.mainBackground}>
-        <View>
+        <View style={{ width: "100%", alignItems: 'center', marginTop: 102, flex: 1 }}>
           <View>
             <Text style={FontStyles.Header}>Success</Text>
             <Text style={FontStyles.MediumText}>Contact has been added successfully.</Text>
-
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={FontStyles.MediumText}>Name:</Text>
+              <TextInput placeholder='Their name' placeholderTextColor={Color.text} style={[FontStyles.MediumText, { marginLeft: 10, paddingHorizontal: 8, paddingVertical: 4, backgroundColor: Color.secondary, borderRadius: 10, minWidth: 160 }]} />
+            </View>
           </View>
-
+          <TouchableOpacity style={{ marginTop: 40 }}>
+            <StandardBackground withBorder style={{ borderRadius: 3 }}>
+              <View style={{ width: 212, height: 52, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={FontStyles.StandardText}>Save</Text>
+              </View>
+            </StandardBackground>
+          </TouchableOpacity>
+        </View>
+        <View style={{ width: "100%", alignItems: 'center', marginBottom: 20 }}>
+          <Logo />
         </View>
       </StandardBackground>
     </SafeAreaView>
