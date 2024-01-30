af1a885c75c50d08be811f5fede2624e318b8c1e
jakubszamuk <lavaproofJakub1@gmail.com>
2024-01-30 22:17:44 +0000

    module created

---
+++ modules/digitalis-share/android/src/main/java/expo/modules/digitalisshare/DigitalisShareView.kt
@@ -0,0 +1,7 @@
+package expo.modules.digitalisshare
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class DigitalisShareView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
---
+++ modules/digitalis-share/index.ts
@@ -0,0 +1,26 @@
+import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+
+// Import the native module. On web, it will be resolved to DigitalisShare.web.ts
+// and on native platforms to DigitalisShare.ts
+import DigitalisShareModule from './src/DigitalisShareModule';
+import DigitalisShareView from './src/DigitalisShareView';
+import { ChangeEventPayload, DigitalisShareViewProps } from './src/DigitalisShare.types';
+
+// Get the native constant value.
+export const PI = DigitalisShareModule.PI;
+
+export function hello(): string {
+  return DigitalisShareModule.hello();
+}
+
+export async function setValueAsync(value: string) {
+  return await DigitalisShareModule.setValueAsync(value);
+}
+
+const emitter = new EventEmitter(DigitalisShareModule ?? NativeModulesProxy.DigitalisShare);
+
+export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
+  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+}
+
+export { DigitalisShareView, DigitalisShareViewProps, ChangeEventPayload };
