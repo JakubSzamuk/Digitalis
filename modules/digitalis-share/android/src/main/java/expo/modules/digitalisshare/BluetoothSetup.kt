dc06996c6b36e0b3f8474158dc3175b4209e70c0
jakubszamuk <lavaproofJakub1@gmail.com>
2024-02-04 16:39:09 +0000

    permissions

--- modules/digitalis-share/android/src/main/java/expo/modules/digitalisshare/BluetoothSetup.kt
+++ modules/digitalis-share/android/src/main/java/expo/modules/digitalisshare/BluetoothSetup.kt
@@ -1,4 +1,5 @@
 package expo.modules.digitalisshare
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
@@ -10,22 +11,66 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import expo.modules.kotlin.AppContext
 import java.io.IOException
 import java.lang.Error
 import java.util.UUID
+import android.location.LocationManager
+import android.provider.Settings
 
-@SuppressLint("MissingPermission")
 class BluetoothSetup {
     val ACTION_REQUEST_ENABLE = 1;
     val ACTION_FOUND = 4;
     val NAME = "Digitalis"
     val MY_UUID = UUID.randomUUID();
 
-
     var AppActivity: Activity? = null;
     var BluetoothCreatedAdapter: BluetoothAdapter? = null;
+
+    val multiplePermissionLauncher =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                    Log.i(MY_TAG, "Launcher result: $permissions")
+                    if (permissions.containsValue(false)) {
+                        Log.i(MY_TAG, "At least one of the permissions was not granted.")
+                        Toast.makeText(
+                                context,
+                                "At least one of the permissions was not granted. Go to app settings and give permissions manually",
+                                Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        //do something
+                        viewModel.scanForDevices()
+                    }
+                }
+            } else {
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                    Log.i(MY_TAG, "Launcher result: $permissions")
+                    if (permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false)) {
+                        //permission for location was granted.
+                        //we direct the user to select "Allow all the time option"
+                        Toast.makeText(
+                                context,
+                                "You must select the option 'Allow all the time' for the app to work",
+                                Toast.LENGTH_SHORT
+                        ).show()
+                        extraLocationPermissionRequest()
+                    } else {
+                        Toast.makeText(
+                                context,
+                                "Location permission was not granted. Please do so manually",
+                                Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+
+
+
 
 
 
@@ -74,10 +119,9 @@ class BluetoothSetup {
 
 
     private val receiver = object : BroadcastReceiver() {
-        @SuppressLint("MissingPermission")
         override fun onReceive(context: Context, intent: Intent) {
             val action: String? = intent.action
-            println("Hello world")
+            println("onRecieve 79")
             when(action) {
                 BluetoothDevice.ACTION_FOUND -> {
                     // Discovery has found a device. Get the BluetoothDevice
@@ -88,35 +132,60 @@ class BluetoothSetup {
                     val deviceUUid = device?.uuids
                     val deviceHardwareAddress = device?.address // MAC address
                     println(deviceHardwareAddress)
-                    println("Hello world")
+                    println("Found device address 90")
                 }
             }
         }
     }
 
 
-    @SuppressLint("MissingPermission")
     fun onCreate(appContext: AppContext) {
+        println("line 99")
+
         AppActivity = appContext.activityProvider?.currentActivity
+        println("line 102")
+
         val applicationContext = AppActivity?.applicationContext
+        println("line 105")
 
         val bluetoothManager: BluetoothManager? = applicationContext?.getSystemService(BluetoothManager::class.java)
+        println("line 108")
+
         BluetoothCreatedAdapter = bluetoothManager?.getAdapter();
+        println("line 111")
 
         if (BluetoothCreatedAdapter == null) {
 //            return Error("No Bluetooth support On this device");
         }
 
         if (BluetoothCreatedAdapter?.isEnabled == false) {
+            println("line 118")
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            println("line 120")
+            if (applicationContext?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.BLUETOOTH_CONNECT) } != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
 
             AppActivity?.startActivityForResult(enableBtIntent, this.ACTION_REQUEST_ENABLE);
+            println("line 133")
+
+        } else {
+            println("Hey, Bluetooth is enabled.")
         }
+
+
     }
     fun startDiscovery() {
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND);
         AppActivity?.registerReceiver(receiver, filter);
-        println("Hello world")
+        println("Discovery began 118")
     }
 
     fun stopDiscovery() {
